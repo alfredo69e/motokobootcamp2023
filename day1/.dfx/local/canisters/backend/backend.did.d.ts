2aea8bff6bd8550a4117263a84a611bd5eb9fb6b
Alfredo Echeverria <alfredo.echeverria@start-fleet.com>
2023-01-17 08:40:41 -0500

    challenge day one completed

---
+++ day1/.dfx/local/canisters/backend/backend.did.d.ts
@@ -0,0 +1,7 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+
+export interface _SERVICE {
+  'multiply' : ActorMethod<[bigint, bigint], bigint>,
+  'square' : ActorMethod<[bigint, bigint], number>,
+}
